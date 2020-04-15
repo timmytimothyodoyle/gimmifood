3307ec1b7c3026113bc647447a5fa19cc6b9b81b
timmyodoyle <sebastian.venere@gmail.com>
2020-04-15 18:57:52 +0200

    maya chupala

--- src/main/java/com/timmy/odoyle/gimmifood/service/impl/SolrFoodQueryService.java
+++ src/main/java/com/timmy/odoyle/gimmifood/service/impl/SolrFoodQueryService.java
@@ -5,11 +5,12 @@ import java.util.List;
 import com.timmy.odoyle.gimmifood.model.material.Food;
 import com.timmy.odoyle.gimmifood.service.FoodQueryService;
 
-public class MockFoodQueryService implements FoodQueryService {
+public class SolrFoodQueryService implements FoodQueryService {
 
 	@Override
 	public Food get(String name) {
-		return new Food();
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
