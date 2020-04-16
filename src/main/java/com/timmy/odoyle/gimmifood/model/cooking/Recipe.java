4f40fba524a6f13c25cd6b59f01f75198a41db0b
maiavenere <venere.maia@gmail.com>
2020-04-16 10:54:27 -0300

    agrego metodos en recipe

--- src/main/java/com/timmy/odoyle/gimmifood/model/cooking/Recipe.java
+++ src/main/java/com/timmy/odoyle/gimmifood/model/cooking/Recipe.java
@@ -19,4 +19,22 @@ public class Recipe extends AbstractItem {
 	private List<Ingredient> ingredients;
 
 	private List<Instruction> instructions;
+	
+	public Recipe (String id, String name, String description, Difficulty difficulty) {
+		super(id,name);
+		this.difficulty = difficulty;
+		this.description = description;
+	}
+	
+	public void addUtensil(Utensil u) {
+		this.utensils.add(u);
+	}
+	
+	public void addIngredient(Ingredient i) {
+		this.ingredients.add(i);
+	}
+	
+	public void addInstructions(Instruction i) {
+		this.instructions.add(i);
+	}
 }
