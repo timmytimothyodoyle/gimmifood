09722c71dd482a2bd6039f83f16f0b3d0da0c522
maiavenere <venere.maia@gmail.com>
2020-04-16 18:50:33 -0300

    no se repiten los chips

--- src/main/webapp/js/search-ingredient.js
+++ src/main/webapp/js/search-ingredient.js
@@ -1,3 +1,5 @@
+let ingrediente = [];
+
 $('#search-btn').click(function(event) {
 	$.post('/food', {
 		popo : 'aasdsd',
@@ -26,18 +28,27 @@ $(document).ready(function(){
 
 
 function success(data) {
-let foods = JSON.parse(data);
-console.log(data);
-console.log(JSON.stringify(JSON.parse(data)[0].name));
-for(let i=0; i<foods.length; i++){
-$('#selected-ingredients').append("<div class='chip'>"+foods[i].name+" <i class='close material-icons'>close</i></div>");
-}
 
+	let foods = JSON.parse(data);
+	console.log(data);
+	console.log(JSON.stringify(JSON.parse(data)[0].name));
+	for(let i=0; i<foods.length; i++){
+	if(!ingrediente.includes(JSON.stringify(foods[i].name))){
+		ingrediente.push(JSON.stringify(foods[i].name));
+	 $('#selected-ingredients').append("<div class='chip' id='"+"ingredient"+foods[i].id+"'>"+foods[i].name+" <i class='close material-icons'>close</i></div>");
+	}
+		}
+		
 };
 
+	
 function error(data) {
 	console.log("culo");
 	console.log(JSON.stringify(data));
 };
 
+
+
+
+ //	$('#selected-ingredients').append("<div class='chip'>"+foods[i].name+" <i class='close material-icons'>close</i></div>");
  
