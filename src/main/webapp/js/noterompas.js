bc16857114974f9d00a4dedb66b287f7b93d1570
maiavenere <venere.maia@gmail.com>
2020-04-16 13:48:04 -0300

    ajax

--- src/main/webapp/js/noterompas.js
+++ src/main/webapp/js/noterompas.js
@@ -1,23 +1,31 @@
 
 
 
-document.querySelector("#js_search").addEventListener("load2");
-let name = document.querySelector("#autocomplete-input").value;
-  
-  
-  let data = {
-    "thing": {
-      "nombre": name
+document.querySelector('#autocomplete-input').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+    let name = document.querySelector("#autocomplete-input").value;
+      let data = {
+      "food": name
     }
-// TODO TIRA ESTO, A VOS TE ANDA? Uncaught SyntaxError: Unexpected token 'function'
-function load2(){
+    console.log(data);
+	load2(data);
+    }
+});
+
+  
+  
+
+// TODO TIRA ESTO, A VOS TE ANDA? Uncaught SyntaxError:function 
+function load2(data){
+console.log(data);
+alert(JSON.stringify(data));
 fetch("/food",{
-   method: 'POST', 
-   headers: {
-       'Content-Type': 'application/json'
-   },
-   body: JSON.stringify(data)
-}).then(function(r){
+   "method": "POST",
+    "mode": 'cors',
+    "headers": { "Content-Type": "application/json" },
+    "body": JSON.stringify(data)
+}
+).then(function(r){
    return r.json()
 })
 .then(function(json) {
